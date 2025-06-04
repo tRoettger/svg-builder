9d3e26ed442a26b9ef7a459a4eaa594ecde6a1e3
tRoettger <tobias.j.roettger@gmail.com>
2025-06-04 19:54:22 +0200

    #1 adds rendered html page

--- src/main/js/main.js
+++ src/main/js/main.js
@@ -1,1 +1,12 @@
 console.log("Hello from Electron");
+
+const {app, BrowserWindow } = require('electron');
+
+const createWindow = () => {
+    const window = new BrowserWindow({
+        width: 800, height: 600
+    });
+    window.loadFile("index.html");
+};
+
+app.whenReady().then(createWindow);
